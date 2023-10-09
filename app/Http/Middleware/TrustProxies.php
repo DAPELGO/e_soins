b222af5a84b555d1a5a070c2bd574033f43dff1e
Abdoul Aziz WELGO <abdoulazizwelgo@gmail.com>
2023-10-09 21:29:25 +0000

    Add trusted proxie

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '197.239.79.146'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
